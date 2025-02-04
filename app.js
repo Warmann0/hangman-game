0517afb7e18f94b7eb803b508cd8521efa064d85
warmann0 <mnw@warmann.org>
2025-02-04 10:42:25 -0700

    pseudocode and flowchart deployed

---
+++ app.js
@@ -0,0 +1,54 @@
+/*
+1. Start the game:
+   - Make/find a list of words to choose from
+   - Pick a random word from the list
+   - Hide the word by showing underscores _ for each letter
+   - Set a limit for wrong guesses
+   - Make empty lists to keep track of correct and wrong guesses
+   - Show everything on the main screen
+
+2. Set up the screen:
+   - Add buttons or allow keyboard input for guessing the letters
+   - Show how many tries are left.
+   - Have a figure or something to show progress.
+
+3. When a letter is guessed:
+   - Check if the letter was already guessed
+   - If the letter is in the word
+     - Show it in the correct spots
+     - Check if the whole word is guessed = User wins
+   - If the letter is NOT in the word:
+     - Take away one try
+     - Update the figure/progress visually d
+     - If no tries are left = User loses
+
+4. Winning or losing:
+   - If the player figures out the word show a "yayzz!" message
+   - If the player runs out of tries show "you suck" and reveal the word
+   - Add a button to restart the game
+
+5. Restarting:
+   - Reset everything
+   - Pick a new word
+   - Empty the guessed letters
+   - Reset the screen
+   - allow the player to try again
+
+*/  
+
+
+
+
+/*-------------------------------- Constants --------------------------------*/
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
