bcb5f7dc43001844ec5691cd6ae48981aafdc987
warmann0 <mnw@warmann.org>
2025-02-07 20:45:46 -0700

    CSS optimization

--- js/app.js
+++ js/app.js
@@ -46,6 +46,8 @@ for (let i = 97; i <= 122; i++) {
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(i)
     keyboardDiv.appendChild(button)
+
+    
  }
  
  
