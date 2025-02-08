91a44fe9503e1d820ab418abdcd9e42b43b57f58
warmann0 <mnw@warmann.org>
2025-02-08 11:38:47 -0700

    app.js updated with more functions

--- js/app.js
+++ js/app.js
@@ -40,16 +40,27 @@
 
 
 /*-------------------------------- Constants --------------------------------*/
+const wordDisplay = document.querySelector(".word-display")
+
 const keyboardDiv = document.querySelector(".keyboard")
 
+
+const getRandomWord = () => {
+   // selecting a word and hint from wordList
+   const {word, hint} = wordList[Math.floor(Math.random() * wordList.length)];
+   console.log(word)
+   document.querySelector(".hint-text b").innerText = hint;
+   wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
+}
+
+// creates keyboard buttons
 for (let i = 97; i <= 122; i++) {
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(i)
     keyboardDiv.appendChild(button)
-
-    
  }
  
+ getRandomWord ();
  
  /*---------------------------- Variables (state) ----------------------------*/
  
