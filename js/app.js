f08d8c4396cda33285b251492f28b257bdd32d57
warmann0 <mnw@warmann.org>
2025-02-07 17:43:54 -0700

    bugs fixed

--- js/app.js
+++ js/app.js
@@ -40,8 +40,12 @@
 
 
 /*-------------------------------- Constants --------------------------------*/
+const keyboardDiv = document.querySelector(".keyboard")
+
 for (let i = 97; i <= 122; i++) {
-    console.log(String.fromCharCode(i));
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i)
+    keyboardDiv.appendChild(button)
  }
  
  
